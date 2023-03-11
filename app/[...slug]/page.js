10b793dfcd8c76e1815ef2e9c16af661bc62079e
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-03-11 13:35:20 +0100

    styling 404-page

--- app/[...slug]/page.js
+++ app/[...slug]/page.js
@@ -5,10 +5,11 @@ export default function NotFoundPage() {
   return (
     <>
       <div className={styles.container}>
-        <h1 className={styles.title}>Oops...</h1>
-        <h2 className={styles.subtitle}>That page cannot be found.</h2>
+        <h2 className={styles.subtitle}>Oops...</h2>
+        <h1 className={styles.title}>404</h1>
+        <p className={styles.subtitle}>That page cannot be found.</p>
         <div className={styles.link}>
-          <p className={styles.para}>Go back to the <Link href='/'>Homepage</Link></p>
+          <p className={styles.para}>Go back to the <Link href='/'>↳ Homepage</Link></p>
         </div>
       </div>
     </>
