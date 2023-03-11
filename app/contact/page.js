f02f22f6f38fdf6be9acf85f39a65aa9f1da4b37
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-03-11 15:09:59 +0100

    remove heading

--- app/contact/page.js
+++ app/contact/page.js
@@ -3,9 +3,9 @@ import styles from '../../styles/Contact.module.css'
 export default function Contact() {
   return (
     <div className={styles.container} style={{ marginTop: "-70px", marginBottom: "-92px", background: '#B7E0E6' }}>
-      <div className={styles.pageTitle}>
+      {/* <div className={styles.pageTitle}>
         <h1 className={styles.heading}>Let's grab a coffee!</h1>
-      </div>
+      </div> */}
       <form className={styles.form} action="mailto:jenny.nguyen_oberg@edu.cmeducations.se?subject=Hej!" method="post">
         <label className={styles.label}>Name</label>
         <input className={styles.inputSmall} placeholder="Jane Doe" />
