27e10ace70e596534bde5f7343f7fed78ab1f19f
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-03-11 15:07:15 +0100

    add links to moreProj

--- app/projects/moreProj.js
+++ app/projects/moreProj.js
@@ -1,8 +1,10 @@
+import Link from 'next/link'
 import styles from '../../styles/Projects.module.css'
 
 export default function MoreProjects() {
   return (
     <div className={styles.moreProjects}>
+      {/* -------  Project description ------- */}
       <div className={styles.subTitle}>More projects</div>
       <span className={styles.contentTitle}>
         <span className={styles.contentDesc}>Year</span>
@@ -11,20 +13,30 @@ export default function MoreProjects() {
         <span className={styles.contentDesc}></span>
       </span>
       <span className={styles.bar}></span>
-      <span className={styles.contentProject}>
-        <span className={styles.contentYear}>2020</span>
-        <span className={styles.contentRole}>Brand Identity</span>
-        <span className={styles.contentClient}>Trygghetsbyrån</span>
-        <span className={styles.contentArrow}>&#x261E;</span>
-      </span>
+
+      {/* -------  Project alt1 ------- */}
+      <Link href='https://www.behance.net/gallery/99425505/Trygghetsbyran'>
+        <span className={styles.contentProject}>
+          <span className={styles.contentYear}>2020</span>
+          <span className={styles.contentRole}>Brand Identity</span>
+          <span className={styles.contentClient}>Trygghetsbyrån</span>
+          <span className={styles.contentArrow}>&#x261E;</span>
+        </span>
+      </Link>
       <span className={styles.bar}></span>
-      <span className={styles.contentProject}>
-        <span className={styles.contentYear}>2018&#8212;2019</span>
-        <span className={styles.contentRole}>Packaging & Brand Identity</span>
-        <span className={styles.contentClient}>Moss & Noor</span>
-        <span className={styles.contentArrow}>&#x261E;</span>
-      </span>
+
+      {/* -------  Project alt2 ------- */}
+      <Link href='https://www.behance.net/gallery/99526981/Moss-Noor'>
+        <span className={styles.contentProject}>
+          <span className={styles.contentYear}>2018&#8212;2019</span>
+          <span className={styles.contentRole}>Packaging & Brand Identity</span>
+          <span className={styles.contentClient}>Moss & Noor</span>
+          <span className={styles.contentArrow}>&#x261E;</span>
+        </span>
+      </Link>
       <span className={styles.bar}></span>
+
+      {/* -------  Project alt3 ------- */}
       <span className={styles.contentProject}>
         <span className={styles.contentYear}>2017</span>
         <span className={styles.contentRole}>Brand Strategist</span>
